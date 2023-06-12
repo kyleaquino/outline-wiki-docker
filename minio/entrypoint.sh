c2981b43344caaa711c4e0e9565708efb7b83661
Kyle Aquino <kshaunaquino@gmail.com>
2023-06-13 02:19:22 +0800

    Update

--- minio/entrypoint.sh
+++ minio/entrypoint.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env sh
 # wrapper for docker entrypoint that takes into account the PORT env var
 
-# Install MinIO Command Line Tools
-curl https://dl.min.io/client/mc/release/linux-amd64/mc \
-  --create-dirs \
-  -o /opt/render/minio-binaries/mc
-
-chmod +x /opt/render/minio-binaries/mc
-export PATH=$PATH:/opt/render/minio-binaries/
-
 # Create a bucket and set access policy
 export MINIO_BUCKET=minio-bucket
 export MINIO_POLICY_FILE=/tmp/policy.json
 cp /opt/render/policy.json $MINIO_POLICY_FILE
 
 # Initialize the MinIO server and create the bucket
-mc config host add myminio http://localhost:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+mc config host add myminio http://127.0.0.1:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 mc mb myminio/$MINIO_BUCKET
 mc policy set myminio/$MINIO_BUCKET $MINIO_POLICY_FILE
 
