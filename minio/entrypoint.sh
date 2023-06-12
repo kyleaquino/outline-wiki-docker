90b73d5942d5d0b04d60f5089422922fc41ef53a
Kyle Aquino <kshaunaquino@gmail.com>
2023-06-13 02:52:04 +0800

    Update

--- minio/entrypoint.sh
+++ minio/entrypoint.sh
@@ -15,8 +15,8 @@ export MINIO_POLICY_FILE=/tmp/policy.json
 cp /opt/render/policy.json $MINIO_POLICY_FILE
 
 # Initialize the MinIO server and create the bucket
-mc config host add myminio http://127.0.0.1:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
-mc mb myminio/$MINIO_BUCKET
-mc policy set myminio/$MINIO_BUCKET $MINIO_POLICY_FILE
+mc alias set local http://localhost:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+mc mb local/$MINIO_BUCKET
+mc policy set local/$MINIO_BUCKET $MINIO_POLICY_FILE
 
 exec docker-entrypoint.sh server --console-address ":10000" --address ":9000" /data
